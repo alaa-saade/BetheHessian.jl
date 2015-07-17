aa24355b8bbd3961f6e055d2686ad4bff4040e0c
Alaa Saade <alaa.saade@m4x.org>
2015-07-17 19:37:21 +0200

    Add small test for matrix completion

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,13 @@ using BetheHessian
 using Base.Test
 
 # write your own tests here
-# Testing matrix completion
-@test demo_MC(n=10,m=10,rank=1,epsilon=10)
+println("Testing Matrix Completion")
+MC_handler(r::Test.Success) = println("Matrix Completion working")
+MC_handler(r::Test.Failure) = error("Matrix Completion not working")
+MC_handler(r::Test.Error) = rethrow(r)
+
+Test.with_handler(MC_handler) do
+	@test demo_MC(force_rank = true,n=10,m=10,rank=1,epsilon=10,verbose=false)<1
+end
+
 
